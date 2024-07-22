43379c35fec48b3647bfc54612d561bcb1033df3
Jefte Keller <jeftekeller.dev@gmail.com>
2024-07-22 16:16:46 -0300

    Add install script for "dir_colors"

---
+++ install/install-dir_colors.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [ -z "$DOTFILES" ] ; then
+  export DOTFILES="$HOME/.dotfiles"
+fi
+
+
+mkdir -p "$HOME/.dir_colors/"
+
+ln -sf "$DOTFILES/dir_colors/dircolors" "$HOME/.dir_colors/dircolors"
