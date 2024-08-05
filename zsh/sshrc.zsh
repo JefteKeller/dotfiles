d6137ce6c51a668e8d2cd13a10eabb138878fb9e
Jefte Keller <jeftekeller.dev@gmail.com>
2024-08-05 02:23:41 -0300

    SSH: Remove unused configs

--- zsh/sshrc.zsh
+++ zsh/sshrc.zsh
@@ -4,9 +4,6 @@
 
 # Settings to make ssh-agent store passphrases
 
-#
-# Option 1 - Works
-#
 
 # Define a file to store the SSH agent environment variables
 SSH_ENV="$HOME/.ssh/environment"
@@ -33,48 +30,9 @@ else
     start_agent;
 fi
 
-# if [ -n "$DESKTOP_SESSION" ];then
-#     eval $(gnome-keyring-daemon --start)
-#     export SSH_AUTH_SOCK
-# fi
-
 
 #
-# Option 2 - Asks for password every new terminal
-#
-
-# if [ ! -f ~/.ssh/.agent ]; then
-#     ssh-agent > ~/.ssh/.agent
-#     ssh-add
-# fi
-# . ~/.ssh/.agent > /dev/null 2>&1
-# if [ "`pidof ssh-agent`" != "$SSH_AGENT_PID" ]; then
-#     unset SSH_AUTH_SOCK
-#     unset SSH_AGENT_PID
-#     ssh-agent > ~/.ssh/.agent
-#     . ~/.ssh/.agent > /dev/null 2>&1
-#     ssh-add
-# fi
-# echo Agent PID $SSH_AGENT_PID
-
-
-#
-# Not sure if will work - For testing
-#
-
-# /usr/bin/keychain --nogui --quiet ~/.ssh/id_ed25519
-# source $HOME/.keychain/Win10-Keller-PC-sh
-
-
-#
-# Test 2 (Seems OK) *Promising*
-#
-
-# eval `keychain --quiet --eval --agents ssh id_ed25519`
-
-
-#
-# Test 3 - Multiple Keys
+# Test - Multiple Keys
 #
 
 # eval `ssh-add ~/.ssh/id_ed25519`
