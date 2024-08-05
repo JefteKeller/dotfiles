a9938433a3d4b95ba8cadf97073065f671460c95
Jefte Keller <jeftekeller.dev@gmail.com>
2024-08-05 02:28:08 -0300

    SSH: Improve spacing between commands

--- zsh/sshrc.zsh
+++ zsh/sshrc.zsh
@@ -11,17 +11,22 @@ SSH_ENV="$HOME/.ssh/environment"
 # Function to start ssh-agent and save its environment variables
 function start_agent {
     echo "Initializing new SSH agent..."
+
     # Start new ssh-agent, with the environment variables stored in SSH_ENV
     ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+
     echo "succeeded"
+
     chmod 600 "${SSH_ENV}"
     . "${SSH_ENV}" > /dev/null
+
     # ssh-add  # Asks for password when opening a shell
 }
 
 # Source SSH settings, if applicable
 if [ -f "${SSH_ENV}" ]; then
     . "${SSH_ENV}" > /dev/null
+
     # Check if the agent is running
     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
         start_agent;
